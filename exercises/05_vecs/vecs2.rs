c7e9191923f452dada73fc43cd4a0ca38f30e70a
filpedroso <filpedroso3@gmail.com>
2025-09-09 19:13:41 +0100

    on structs

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -2,10 +2,10 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     let mut output = Vec::new();
 
     for element in input {
-        // TODO: Multiply each element in the `input` slice by 2 and push it to
+        output.push(element * 2);
+        // Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
     }
-
     output
 }
 
@@ -24,7 +24,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+            element * 2
         })
         .collect()
 }
